09922544848371febf18e31321fa5b2a1e633b40
Gilles Helleu <helleugilles@gmail.com>
2023-10-31 16:44:08 +0100

    modification de jeu.py

--- jeu.py
+++ jeu.py
@@ -1,11 +1,35 @@
 from modules.characters import Characters
 from modules.ennemi import Ennemi
 from modules.personnage import Personnage
-from main import pop_ennemi
 import time
 
+import random
 
-def main():
+
+def pop_ennemi() -> Ennemi:
+
+    ennemis = [
+        {"nom": "Gobelin",
+         "pv": 50,
+         "dega_min": 10,
+         "dega_max": 20},
+        {"nom": "Orc",
+         "pv": 75,
+         "dega_min": 10,
+         "dega_max": 20},
+        {"nom": "Dragon",
+         "pv": 100,
+         "dega_min": 10,
+         "dega_max": 20},
+    ]
+
+    ennemi_number = random.randint(0, len(ennemis)-1)
+    ennemi = Ennemi(ennemis[ennemi_number]["nom"], ennemis[ennemi_number]["pv"],
+                    ennemis[ennemi_number]["dega_min"], ennemis[ennemi_number]["dega_max"])
+    return ennemi
+
+
+def jeu():
     personnage = Personnage("Hero")
     print(personnage)
     ennemi = pop_ennemi()
@@ -33,7 +57,7 @@ def main():
                     time.sleep(1)
                     break
                 else:
-                    print("merci de rentrer quelque chose de valide")
+                    print("merci de rentrer quelque chose dAe valide")
 
         # l'ennemi attaque juste après
 
@@ -61,4 +85,4 @@ def main():
             # faire rejouer
 
 
-main()
+jeu()
