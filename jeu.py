3316f07b69289cd2fdc4b5cfcfceebdf82f937a0
Gilles Helleu <helleugilles@gmail.com>
2023-10-31 15:35:24 +0100

    changement jeu avec mort

--- jeu.py
+++ jeu.py
@@ -5,9 +5,9 @@ import time
 
 
 def main():
-    personnage = Personnage("Hero", 150, 10, 20)
+    personnage = Personnage("Hero", 40, 10, 20)
     print(personnage)
-    ennemi = Ennemi("Gobelin", 150, 10, 20)
+    ennemi = Ennemi("Gobelin", 40, 10, 20)
     print(ennemi)
 
     while True:
@@ -16,17 +16,26 @@ def main():
             "que voulez-vous faire : \n A - Attaquer \n B - rechercher un objet \n C - utiliser un objet \n REPONSE :  ").lower()
         if user_ask == "a":
             personnage.attaquer(ennemi)
-            time.sleep(3)
-
-            print("Oh non... l'ennemi vous attaque !!!!!! ***************************")
-            ennemi.attaquer(personnage)
+            if ennemi.check_life():
+                personnage.add_xp
+        # l'ennemi attaque juste après
 
         elif user_ask == "b":
             personnage.search_object()
-            print("Oh non... l'ennemi vous attaque !!!!!!")
-            ennemi.attaquer(personnage)
 
         time.sleep(3)
+        print("Oh non... l'ennemi vous attaque !!!!!! ***************************")
+        ennemi.attaquer(personnage)
+
+        time.sleep(3)
+        print("*"*8)
+        print(personnage)
+        print(ennemi)
+        print("*"*8)
+
+        time.sleep(3)
+        if personnage.check_life():
+            break
 
 
 main()
