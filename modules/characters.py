9a625a849c6b210fb932e62cfe0cfb0655d6d29f
Gilles Helleu <helleugilles@gmail.com>
2023-10-31 10:37:10 +0100

    changé en ajoutant character qui sera central tout en gardant les fichiers ennemi et personnage

--- modules/characters.py
+++ modules/characters.py
@@ -1,7 +1,11 @@
+class Ennemi:
+    pass
+
+
 class Personnage:
     def __init__(self, nom, pv) -> None:
         self.nom = nom
         self.pv = pv
 
     def attaquer(self):
-        p
+        pass
