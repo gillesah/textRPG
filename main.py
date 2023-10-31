81761332c9127b70c9e534fe9a584d28071698f2
Rachel Williams <rachelwilliams@mailbox.org>
2023-10-31 13:35:52 +0100

    check_life() => classe Characters

--- main.py
+++ main.py
@@ -2,23 +2,16 @@ from modules.characters import Characters
 from modules.ennemi import Ennemi
 from modules.personnage import Personnage
 
-
-def check_life(character: Characters):
-    if character.pv < 0:
-        print(f"{character.nom} est mort")
-        return True
-
-
 def main():
     personnage = Personnage("Hero", 150, 10, 20)
     ennemi = Ennemi("Gobelin", 150, 10, 20)
 
     while True:
         personnage.attaquer(ennemi)
-        if check_life(ennemi):
+        if ennemi.check_life():
             break
         ennemi.attaquer(personnage)
-        if check_life(personnage):
+        if personnage.check_life():
             break
 
 
--- modules/characters.py
+++ modules/characters.py
@@ -10,7 +10,12 @@ class Characters(ABC):
         self.dega_min = dega_min
         self.deg_max = dega_max
 
-    def attaquer(self, character):
-        pa = random.randint(character.dega_min, character.deg_max)
-        print(f"{self.nom} attaque {character.nom} et lui inflige {pa}")
-        character.pv -= pa
+    def attaquer(self, cible):
+        pa = random.randint(cible.dega_min, cible.deg_max)
+        print(f"{self.nom} attaque {cible.nom} et lui inflige {pa}")
+        cible.pv -= pa
+        
+    def check_life(self):
+        if self.pv < 0:
+            print(f"{self.nom} est mort")
+            return True
