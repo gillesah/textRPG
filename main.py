6a4c052f523ebab31081dc5b4a055f2aea331188
Guillaume Lemaitre <guillaume.lemaitre1@gmail.com>
2023-10-31 11:36:56 +0000

    mvp

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+from modules.characters import Characters
 from modules.ennemi import Ennemi
 from modules.personnage import Personnage
+
+
+def check_life(character: Characters):
+    if character.pv < 0:
+        print(f"{character.nom} est mort")
+        return True
+
+
+def main():
+    personnage = Personnage("Hero", 150, 10, 20)
+    ennemi = Ennemi("Gobelin", 150, 10, 20)
+
+    while True:
+        personnage.attaquer(ennemi)
+        if check_life(ennemi):
+            break
+        ennemi.attaquer(personnage)
+        if check_life(personnage):
+            break
+
+
+main()
